53b2e755eb3def0f6f959e06db45c992a4f64189
Adrián Rincón López <arincon.developer@gmail.com>
2021-06-22 16:28:09 +0200

    Revert XML

--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/config/WebSecurityConfig.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/config/WebSecurityConfig.java
@@ -258,7 +258,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements I
     @Qualifier("idp-ssocircle")
     public ExtendedMetadataDelegate ssoCircleExtendedMetadataProvider()
         throws MetadataProviderException {
-        String idpSSOCircleMetadataURL = "https://saml-rediris-example.herokuapp.com/saml/metadata";
+        String idpSSOCircleMetadataURL = "https://idp.ssocircle.com/meta-idp.xml";
         HTTPMetadataProvider httpMetadataProvider = new HTTPMetadataProvider(
             this.backgroundTaskTimer, httpClient(), idpSSOCircleMetadataURL);
         httpMetadataProvider.setParserPool(parserPool());
