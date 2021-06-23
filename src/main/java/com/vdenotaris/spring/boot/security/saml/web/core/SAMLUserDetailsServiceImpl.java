c3b3cd57ec68f3125554caedd37b4b9a2d452d56
Adrián Rincón López <arincon.developer@gmail.com>
2021-06-23 13:55:51 +0200

    Info logs

--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/core/SAMLUserDetailsServiceImpl.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/core/SAMLUserDetailsServiceImpl.java
@@ -54,8 +54,7 @@ public class SAMLUserDetailsServiceImpl implements SAMLUserDetailsService {
         // In a real scenario, this implementation has to locate user in a arbitrary
         // dataStore based on information present in the SAMLCredential and
         // returns such a date in a form of application specific UserDetails object.
-        return new User(credential.getAttributes().toString(), credential.getAdditionalData().toString(),
-            true, true, false, true, authorities);
+        return new User(userID, "<abc123>", true, true, true, true, authorities);
     }
 
 }
